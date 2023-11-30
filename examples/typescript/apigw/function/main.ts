737c2bedea4c32c5f131e9fcab8916beadf933a9
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2023-11-30 15:27:23 +0100

    doc: add comments

--- examples/typescript/apigw/function/main.ts
+++ examples/typescript/apigw/function/main.ts
@@ -1,17 +1,24 @@
 import {Http} from '@yandex-cloud/function-types/dist/src/http';
 
+// RequestBody interface represents the structure of the request body.
 interface RequestBody {
-    name: string;
+    name: string; // The name property of the request body.
 }
 
+// The handler function is an asynchronous function that handles HTTP events.
+// It takes a Http.Event object as a parameter and returns a Promise that resolves to a Http.Result object.
 export async function handler(event: Http.Event): Promise<Http.Result> {
 
+    // Parse the body of the event into a RequestBody object.
     const body = JSON.parse(event.body || '{}') as RequestBody;
 
+    // Create a response object with a message property.
     const response = {
         message: `Hello, ${body.name || 'World'}!`
     }
 
+    // Return a Http.Result object with a status code of 200, a 'Content-Type' header set to 'application/json',
+    // and the body set to the JSON stringified version of the response object.
     return {
         statusCode: 200,
         headers: {
