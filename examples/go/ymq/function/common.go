19e5cc2aab7235f78e94de954ef1d465571b2316
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2024-10-01 16:23:28 +0200

    feat(go/ymq): update to use resolver v2

--- examples/go/ymq/function/common.go
+++ examples/go/ymq/function/common.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	smithyendpoints "github.com/aws/smithy-go/endpoints"
 )
 
 func sendMessageToQueue(
@@ -17,25 +19,20 @@ func sendMessageToQueue(
 	origin string,
 	delay int32,
 ) (*sqs.SendMessageOutput, error) {
-	// Create a custom endpoint resolver to resolve Yandex Message Queue endpoint
-	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		return aws.Endpoint{
-			URL:           "https://message-queue.api.cloud.yandex.net",
-			SigningRegion: "ru-central1",
-		}, nil
-	})
 	// Load the SDK's configuration from environment and shared config
 	// In the serverless environment, the configuration is loaded from the environment variables
 	// AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.
 	cfg, err := config.LoadDefaultConfig(
 		ctx,
-		config.WithEndpointResolverWithOptions(customResolver),
 	)
 	if err != nil {
 		panic("configuration error, " + err.Error())
 	}
 	// Create an Amazon SQS service client
-	client := sqs.NewFromConfig(cfg)
+	client := sqs.NewFromConfig(cfg, func(o *sqs.Options) {
+		o.Region = "ru-central1"
+		o.EndpointResolverV2 = &resolverV2{}
+	})
 
 	gQInput := &sqs.GetQueueUrlInput{
 		QueueName: &ymqName,
@@ -61,4 +58,20 @@ func sendMessageToQueue(
 	}
 	resp, err := client.SendMessage(ctx, sMInput)
 	return resp, nil
+}
+
+type resolverV2 struct {
+	// you could inject additional application context here as well
+}
+
+func (*resolverV2) ResolveEndpoint(_ context.Context, _ sqs.EndpointParameters) (
+	smithyendpoints.Endpoint, error,
+) {
+	u, err := url.Parse("https://message-queue.api.cloud.yandex.net")
+	if err != nil {
+		return smithyendpoints.Endpoint{}, err
+	}
+	return smithyendpoints.Endpoint{
+		URI: *u,
+	}, nil
 }
