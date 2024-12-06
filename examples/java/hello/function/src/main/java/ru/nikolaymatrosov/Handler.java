9643b6682fc89d9aaad456c8005634038866fa1d
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2024-12-06 13:22:35 +0100

    fix(java): hello example

--- examples/java/hello/function/src/main/java/ru/nikolaymatrosov/Handler.java
+++ examples/java/hello/function/src/main/java/ru/nikolaymatrosov/Handler.java
@@ -9,10 +9,11 @@ import java.nio.charset.StandardCharsets;
 public class Handler implements YcFunction<Integer, String> {
     @Override
     public String handle(Integer i, Context c) {
-        PrintStream out = null;
-        out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         System.out.println("stdout: Привет, Мир!");
+
+        PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
         out.println("utf8: Привет, Мир!");
+
         return String.valueOf(i);
     }
 }
