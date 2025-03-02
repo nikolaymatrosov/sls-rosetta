59d7117e218ecdc15a669db63f2d1142795bde60
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2025-03-02 14:17:10 +0100

    feat(go): api gw router example (#20)

--- examples/go/apigw/tf/apigateway.tf
+++ examples/go/apigw/tf/apigateway.tf
@@ -2,6 +2,7 @@ resource "yandex_api_gateway" "api_gateway" {
   name = "api-gateway"
   spec = templatefile("./api-gateway.yaml", {
     test_function_id   = yandex_function.test_function.id
+    route_function_id  = yandex_function.route_function.id
     sa_id = yandex_iam_service_account.sa_serverless.id
   })
 }
--- examples/go/apigw/tf/main.tf
+++ examples/go/apigw/tf/main.tf
@@ -7,8 +7,21 @@ data "archive_file" "function_files" {
 resource "yandex_function" "test_function" {
   name               = "api-gateway-demo"
   user_hash          = data.archive_file.function_files.output_sha256
-  runtime            = "golang119"
+  runtime            = "golang121"
   entrypoint         = "index.Handler"
+  memory             = "128"
+  execution_timeout  = "10"
+  service_account_id = yandex_iam_service_account.sa_serverless.id
+  content {
+    zip_filename = data.archive_file.function_files.output_path
+  }
+}
+
+resource "yandex_function" "route_function" {
+  name               = "router-function"
+  user_hash          = data.archive_file.function_files.output_sha256
+  runtime            = "golang121"
+  entrypoint         = "index.Serverless"
   memory             = "128"
   execution_timeout  = "10"
   service_account_id = yandex_iam_service_account.sa_serverless.id
