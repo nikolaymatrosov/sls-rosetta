6d88446334dd9323636599e0d86c4d46b7861684
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2025-09-02 09:22:10 +0200

    feat(go): update Go version to 1.23.9 and integrate structured logging

--- examples/go/hello/function/main.go
+++ examples/go/hello/function/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func Handler(rw http.ResponseWriter, req *http.Request) {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}))
+	// Simple usage
+	logger.Info("hello", "count", 3)
+	logger.Warn("warn message")
+	logger.Error("error message", "key", "value")
+
+	// With context
+	ctx := context.WithValue(req.Context(), "requestID", "12345")
+	logger.DebugContext(ctx, "debug message")
+
 	rw.Header().Set("X-Custom-Header", "Test")
 	rw.WriteHeader(200)
 	name := req.URL.Query().Get("name")
